19c2bd6f06f13e88b78ca0e854a8f1aa7d202de7
Yun Hsiang <yun.hsiang@sifive.com>
2023-12-04 22:14:52 -0800

    Add SiFive copyright

--- samples/custom_dispatch/cpu/plugin/layernorm_uk.h
+++ samples/custom_dispatch/cpu/plugin/layernorm_uk.h
@@ -1,3 +1,21 @@
+// Copyright (c) 2023 SiFive, Inc. -- Proprietary and Confidential All Rights
+// Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// SiFive, Inc. The intellectual and technical concepts contained herein are
+// proprietary to SiFive, Inc. and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+//
+// This work may not be copied, modified, re-published, uploaded, executed, or
+// distributed in any way, in any medium, whether in whole or in part, without
+// prior written permission from SiFive, Inc.
+//
+// The copyright notice above does not evidence any actual or intended
+// publication or disclosure of this source code, which includes information
+// that is confidential and/or proprietary, and is a trade secret, of SiFive,
+// Inc.
+//===----------------------------------------------------------------------===//
+
 #include <riscv_vector.h>
 
 // vector inverse 
